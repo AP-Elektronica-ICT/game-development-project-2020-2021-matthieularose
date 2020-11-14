c630f5eed9c1ca98fa924de22c9cd8ed8b7956c8
Matthieu <matthieu.larose@student.ap.be>
2020-11-14 15:40:20 +0100

    game projectfolder

---
+++ game/GameObjects/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace GameDevProject.GameObjects
+{
+    public class Player
+    {
+        public Player()
+        {
+            Debug.WriteLine("Werkt");
+        }
+    }
+}
