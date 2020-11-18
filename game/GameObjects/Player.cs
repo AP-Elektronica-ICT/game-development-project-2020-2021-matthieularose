e64762b284a6b20bdf639294c0bea2d8975f4903
Matthieu <matthieu.larose@student.ap.be>
2020-11-18 20:58:43 +0100

    Add Player Class

--- game/GameObjects/Player.cs
+++ game/GameObjects/Player.cs
@@ -3,14 +3,31 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Diagnostics;
+using GameDevProject.Interfaces;
+using GameDevProject.Animation;
 
 namespace GameDevProject.GameObjects
 {
-    public class Player
+    public class Player : IGameObject
     {
-        public Player()
+        Texture2D idleTexture;
+
+        SpriteAnimation idleAnimation;
+
+        public Player(Texture2D idle_texture)
         {
-            Debug.WriteLine("Werkt");
+            idleTexture = idle_texture;
+            idleAnimation = new SpriteAnimation(95, 102, 12);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            idleAnimation.Update();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (idleTexture != null)spriteBatch.Draw(idleTexture, new Vector2(10, 10), idleAnimation.currentFrame.sourceRectangle, Color.White);
         }
     }
 }
