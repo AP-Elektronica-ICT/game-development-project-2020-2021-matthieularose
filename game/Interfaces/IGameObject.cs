1bf22673c1d1485ca835970e1ed26acb72c35ab8
Matthieu <matthieu.larose@student.ap.be>
2020-11-18 20:59:48 +0100

    Add IGameObjects Interface

---
+++ game/Interfaces/IGameObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameDevProject.Interfaces
+{
+    public interface IGameObject
+    {
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
