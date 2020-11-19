fcb73a580f91a3a97caa41a8cbccf2de0bbb331a
Matthieu <matthieu.larose@student.ap.be>
2020-11-19 19:39:24 +0100

    ICollision Interface

---
+++ game/Interfaces/ICollision.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace GameDevProject.Interfaces
+{
+    public interface ICollision
+    {
+        Rectangle collidingRect { get; set; }
+    }
+}
