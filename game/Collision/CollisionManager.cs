3d6496262b03df256e9df8cf6b38cf9fc3585310
Matthieu <matthieu.larose@student.ap.be>
2020-11-19 17:46:01 +0100

    CollisionManager Class

---
+++ game/Collision/CollisionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace GameDevProject.Collision
+{
+    public class CollisionManager
+    {
+        public bool IsColliding(Rectangle rect1, Rectangle rect2)
+        {
+            if (rect1.Intersects(rect2)) return true;
+            return false;
+        }
+    }
+}
