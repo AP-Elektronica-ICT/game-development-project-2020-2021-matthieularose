a9df7414af4169c72ed3b986b271cccd603cbf0c
Matthieu <matthieu.larose@student.ap.be>
2021-01-11 16:10:10 +0100

    Abstract GameObject class

---
+++ game/GameObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GameDevProject.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameDevProject
+{
+    public abstract class GameObject : IGameObject
+    {
+        public GameObject()
+        {
+        }
+
+        public abstract Vector2 Position { get; set; }
+        public abstract Rectangle CollisionRectangle { get; set; }
+
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(SpriteBatch spriteBatch);
+    }
+}
